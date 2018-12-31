6581a5bbb2444e5e5012fab75e1d73563cd22f91
Eduardo Reyes <tred27@gmail.com>
2018-12-31 17:46:24 -0600

    feat(leaderboard): Add the logic required for the course leaderboard to appear
    Adds a course leaderboard that's broken down into two columns, one that's a country scoped
    leaderboard and another one that's a global leaderboard, each one of these columns it's broken down
    into three sections, one with the top three performers, the second one with either the middle
    performers or the current user position 1/-1 and the last one is the three last performers, the
    current logged in user appears bolded and can be in either one of the sections but will appear in
    both columns all the time.

--- app/Leaderboard.php
+++ app/Leaderboard.php
@@ -35,4 +35,62 @@ class Leaderboard extends Model
     {
         return $this->belongsTo(Course::class);
     }
+
+    public static function getCourseLeaderboard(Course $course, String $scope = "course"): Array
+    {
+
+        if ("course" == $scope) {
+            $leaderboard = Leaderboard::where('course_id', $course->id)
+                ->orderBy('total_score', 'DESC')
+                ->get();
+        } else {
+            $leaderboard = Leaderboard::where('course_id', $course->id)
+                ->orderBy('total_score', 'DESC')
+                ->with('user')
+                ->get();
+
+            $user_country = optional(\Auth::user())->country_id ?? null;
+
+            if (isset($user_country)) {
+                $leaderboard = $leaderboard->filter(function($entry) use ($user_country) {
+                    return $entry->user->country->id == $user_country;
+                })->values();
+            }
+        }
+
+        $currentLeaderboardPosition = (new self)->getCurrentLeaderboardPosition($leaderboard);
+
+        $topLeaderboard = $leaderboard->slice(0,3);
+        $lastLeaderboard = $leaderboard->slice(-3,3);
+
+        // Determine if the current user is not on the top or bottom
+        if ($currentLeaderboardPosition < 3 || $currentLeaderboardPosition > (count($leaderboard) - 4)) {
+            $currentLeaderboard = $leaderboard->slice(ceil(count($leaderboard) / 2), 3);
+        } else {
+            $currentLeaderboard = $leaderboard->slice($currentLeaderboardPosition - 1, 3);
+        }
+
+        $numberFormatter = new \NumberFormatter('en_US', \NumberFormatter::ORDINAL);
+        $currentLeaderboardPosition = $numberFormatter->format($currentLeaderboardPosition + 1);
+
+        return [$topLeaderboard, $currentLeaderboard, $lastLeaderboard, $currentLeaderboardPosition];
+    }
+
+    /**
+     * Gets the current user or passed user position in a given leaderboard.
+     * If no explicit user is passed then it attempts to get the current user.
+     *
+     * @param Collection $leaderboard
+     * @param \App\User $user
+     */
+    private function getCurrentLeaderboardPosition(Collection $leaderboard, User $user = null): int
+    {
+        if (!isset($user) && \Auth::check()) {
+            $user = \Auth::user();
+        } elseif (!\Auth::check()) {
+            return 0;
+        }
+
+        return $leaderboard->where('user_id', $user->id)->keys()->first();
+    }
 }
